148da1482c62730d555e1f2d413518dca4843734
Keronei <lincolnkeronei95@gmail.com>
2022-05-09 10:31:14 +0300

    Refine factory method.

--- app/src/main/java/com/keronei/survey/core/WidgetFactory.kt
+++ app/src/main/java/com/keronei/survey/core/WidgetFactory.kt
@@ -4,17 +4,25 @@ import android.content.Context
 import com.keronei.survey.domain.models.QuestionDefinition
 import com.keronei.survey.presentation.views.QuestionWidget
 import com.keronei.survey.presentation.views.widgets.FloatWidget
+import com.keronei.survey.presentation.views.widgets.SelectSingleWidget
 import com.keronei.survey.presentation.views.widgets.SingleLineStringWidget
 
 class WidgetFactory(private val context: Context) {
     fun createWidgetForQuestion(questionDefinition: QuestionDefinition): QuestionWidget {
-        return when(questionDefinition.answerType){
-            AnswerType.SINGLE_LINE_TEXT -> {
-                SingleLineStringWidget(context, questionDefinition)
+        return when (questionDefinition.questionType) {
+            QuestionType.FREE_TEXT -> {
+                stringWidget(questionDefinition)
             }
-            AnswerType.FLOAT -> {
-                FloatWidget(context, questionDefinition)
+            QuestionType.SELECT_ONE -> SelectSingleWidget(context, questionDefinition)
+            QuestionType.TYPE_VALUE -> {
+                stringWidget(questionDefinition)
             }
         }
     }
+
+    private fun stringWidget(questionDefinition: QuestionDefinition) =
+        when (questionDefinition.answerType) {
+            AnswerType.SINGLE_LINE_TEXT -> SingleLineStringWidget(context, questionDefinition)
+            AnswerType.FLOAT -> FloatWidget(context, questionDefinition)
+        }
 }
