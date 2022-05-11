8bd45cd53ff344f522b1982061d818c640708a0c
Keronei <lincolnkeronei95@gmail.com>
2022-05-11 07:02:01 +0300

    Moved function declaration to correct scope

--- app/src/main/java/com/keronei/survey/domain/repositories/QuestionnaireRepository.kt
+++ app/src/main/java/com/keronei/survey/domain/repositories/QuestionnaireRepository.kt
@@ -1,13 +1,12 @@
 package com.keronei.survey.domain.repositories
 
 import com.keronei.survey.core.AnswerData
+import com.keronei.survey.core.Resource
 import com.keronei.survey.domain.models.QuestionnaireDef
 import kotlinx.coroutines.flow.Flow
 
 interface QuestionnaireRepository {
     fun getQuestionnaireById(string: String): Flow<QuestionnaireDef>
 
-    fun getQuestionnaires(): Flow<List<QuestionnaireDef>>
-
-    fun saveQuestionnaireResponse(questionnaireId: String, answers: List<AnswerData>)
+    suspend fun getQuestionnaires(): Flow<Resource<List<QuestionnaireDef>>>
 }
--- app/src/main/java/com/keronei/survey/domain/repositories/SubmissionsRepository.kt
+++ app/src/main/java/com/keronei/survey/domain/repositories/SubmissionsRepository.kt
@@ -1,10 +1,17 @@
 package com.keronei.survey.domain.repositories
 
+import com.keronei.survey.core.AnswerData
 import com.keronei.survey.domain.models.Submission
 import kotlinx.coroutines.flow.Flow
 
 interface SubmissionsRepository {
     fun getSubmissions(): Flow<List<Submission>>
 
-    fun submitCurrentResponses()
+    suspend fun submitCurrentResponses()
+
+    fun saveQuestionnaireResponse(
+        questionnaireId: String,
+        submissionName: String,
+        answers: List<AnswerData>
+    )
 }
