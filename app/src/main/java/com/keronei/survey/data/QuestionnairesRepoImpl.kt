a48862cb53d0d95e0f3274e725bd17e1cc62f2a5
Keronei <lincolnkeronei95@gmail.com>
2022-05-11 07:01:22 +0300

    Implement data flow in repo impl

--- app/src/main/java/com/keronei/survey/data/QuestionnairesRepoImpl.kt
+++ app/src/main/java/com/keronei/survey/data/QuestionnairesRepoImpl.kt
@@ -1,48 +1,81 @@
 package com.keronei.survey.data
 
-import com.keronei.survey.core.AnswerData
-import com.keronei.survey.core.QuestionType
+import com.keronei.survey.core.Resource
 import com.keronei.survey.data.local.LocalDataSource
 import com.keronei.survey.data.remote.NetworkDataSource
-import com.keronei.survey.domain.models.QuestionDefinition
+import com.keronei.survey.domain.mapper.QuestionnaireDefDtoToQuestionnaireDefMapper
+import com.keronei.survey.domain.mapper.QuestionnaireResponseToQuestionnaireDtoMapper
 import com.keronei.survey.domain.models.QuestionnaireDef
 import com.keronei.survey.domain.repositories.QuestionnaireRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 class QuestionnairesRepoImpl @Inject constructor(
-    val localDataSource: LocalDataSource,
-    val networkDataSource: NetworkDataSource
+    private val localDataSource: LocalDataSource,
+    private val networkDataSource: NetworkDataSource
 ) : QuestionnaireRepository {
+    private val mapper = QuestionnaireDefDtoToQuestionnaireDefMapper()
+
     override fun getQuestionnaireById(string: String): Flow<QuestionnaireDef> {
 
         val result = localDataSource.getQuestionnaireById(string)
 
-        result.map { item ->
-            QuestionnaireDef(
-                item.id,
-                item.questions.size,
-                item.language,
-                item.questions.map { qns -> QuestionDefinition(qns.id, qns.) })
+        return result.map { item ->
+            mapper.map(item)
 
         }
     }
 
-    private fun getQuestionType(value: String): QuestionType {
-        return when (value) {
-            "FREE_TEXT" -> QuestionType.FREE_TEXT
-            "SELECT_ONE" -> QuestionType.SELECT_ONE
-            "TYPE_VALUE" -> QuestionType.TYPE_VALUE
-            else -> QuestionType.FREE_TEXT
+
+    override suspend fun getQuestionnaires(): Flow<Resource<List<QuestionnaireDef>>> =
+        callbackFlow {
+            trySend(Resource.Loading)
+
+            val definitions = localDataSource.getQuestionnaires()
+
+            val local = definitions.map { list ->
+                mapper.mapList(list)
+            }
+
+            local.collect { qns ->
+                if (qns.isEmpty()) {
+                    networkDataSource.getQuestionnaire().collect { remoteResult ->
+                        when (remoteResult) {
+                            is Resource.Failure -> {
+                                trySend(Resource.Failure(remoteResult.exception))
+                            }
+                            Resource.Loading -> {
+                                trySend(Resource.Loading)
+                            }
+                            is Resource.Success -> {
+                                val data = remoteResult.data
+
+                                val forSaving =
+                                    QuestionnaireResponseToQuestionnaireDtoMapper().map(data)
+
+                                localDataSource.addQuestionnaire(
+                                    forSaving.id,
+                                    forSaving.language,
+                                    forSaving.questions,
+                                    forSaving.startQuestionId
+                                )
+                            }
+                        }
+                    }
+
+
+                } else {
+                    trySend(Resource.Success(qns))
+                }
+
+            }
+            awaitClose { close() }
         }
-    }
 
-    override fun getQuestionnaires(): Flow<List<QuestionnaireDef>> {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveQuestionnaireResponse(questionnaireId: String, answers: List<AnswerData>) {
-        TODO("Not yet implemented")
-    }
 }
--- app/src/main/java/com/keronei/survey/data/SubmissionsRepoImpl.kt
+++ app/src/main/java/com/keronei/survey/data/SubmissionsRepoImpl.kt
@@ -1,21 +1,79 @@
 package com.keronei.survey.data
 
+import com.google.gson.Gson
+import com.keronei.survey.core.AnswerData
 import com.keronei.survey.data.local.LocalDataSource
+import com.keronei.survey.data.models.SubmissionsDTO
+import com.keronei.survey.data.models.SubmissionsDTOUpdate
 import com.keronei.survey.data.remote.NetworkDataSource
+import com.keronei.survey.domain.mapper.SubmissionsDtoToSubmissionMapper
+import com.keronei.survey.domain.models.ServerSubmission
 import com.keronei.survey.domain.models.Submission
 import com.keronei.survey.domain.repositories.SubmissionsRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import java.util.*
 import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
 class SubmissionsRepoImpl @Inject constructor(
-    remoteDataSource: NetworkDataSource,
-    localDataSource: LocalDataSource
+    private val remoteDataSource: NetworkDataSource,
+    private val localDataSource: LocalDataSource
 ) : SubmissionsRepository {
     override fun getSubmissions(): Flow<List<Submission>> {
-        TODO("Not yet implemented")
+        val submissions = localDataSource.getSubmissions()
+        return submissions.map { dto ->
+            SubmissionsDtoToSubmissionMapper().mapList(dto)
+        }
     }
 
-    override fun submitCurrentResponses() {
-        TODO("Not yet implemented")
+    override suspend fun submitCurrentResponses() {
+        val submissions = localDataSource.getSubmissions()
+
+        val objectForDispatch = submissions.map { submissionsItem ->
+            ServerSubmission(
+                submissionsItem.first().questionnaireId,
+                Gson().toJson(submissionsItem.map { item -> item.submissionAsJson })
+            )
+        }
+        val ids = submissions.map { updates ->
+            updates.map { entry ->
+                SubmissionsDTOUpdate(entry.id, true)
+            }
+        }
+
+        val item = objectForDispatch.first()
+        val result = remoteDataSource.sendSubmissions(item)
+
+        // mark sync status for dispatched responses
+        val successStatus = result.first()
+        if (successStatus) {
+            val toMark = ids.first()
+            toMark.forEach {
+                localDataSource.markAsSynced(it)
+            }
+        }
+
     }
+
+    override fun saveQuestionnaireResponse(
+        questionnaireId: String,
+        submissionName: String,
+        answers: List<AnswerData>
+    ) {
+        val calendar = Calendar.getInstance()
+        localDataSource.createSubmission(
+            SubmissionsDTO(
+                0,
+                calendar.timeInMillis,
+                questionnaireId,
+                submissionName,
+                Gson().toJson(answers),
+                false
+            )
+        )
+    }
+
 }
