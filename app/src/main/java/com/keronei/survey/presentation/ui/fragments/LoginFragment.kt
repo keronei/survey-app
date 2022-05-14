0bc79984fc8c4f3001cf79dc4e9c19379db94b78
Keronei <lincolnkeronei95@gmail.com>
2022-05-14 09:06:00 +0300

    Add phone regex

--- app/src/main/java/com/keronei/survey/presentation/ui/fragments/LoginFragment.kt
+++ app/src/main/java/com/keronei/survey/presentation/ui/fragments/LoginFragment.kt
@@ -19,6 +19,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -27,6 +28,7 @@ import com.keronei.survey.R
 import com.keronei.survey.databinding.LoginFragmentBinding
 import com.keronei.survey.presentation.ui.viewmodel.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.regex.Pattern
 
 @AndroidEntryPoint
 class LoginFragment : Fragment() {
@@ -52,10 +54,34 @@ class LoginFragment : Fragment() {
     private fun setOnClickListeners() {
         loginBinding.btnLogin.setOnClickListener {
             if (loginBinding.phoneNumberInput.text.isNotEmpty()) {
-                if (loginBinding.passwordInput.text.isNotEmpty()) {
-                    findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+
+                val providedPhone = loginBinding.phoneNumberInput.text.trim()
+
+                val matcher =
+                    Pattern.compile("^\\+254\\d{9}\$").matcher(providedPhone)
+
+                if (matcher.matches()) {
+
+                    if (loginBinding.passwordInput.text.isNotEmpty()) {
+                        findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+
+                        sendEmail()
+                    }
+                } else {
+                    loginBinding.phoneNumberInput.error =
+                        getString(R.string.invalid_phone_message)
+
+                    Toast.makeText(
+                        context,
+                        getString(R.string.invalid_phone_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
         }
+    }
+
+    private fun sendEmail() {
+
     }
 }
